88b2339422d8c4af0b37b0373a811ada7e51efd1
nbt12awt <nbt12awt@07586-STUD-D.HIG-AD.hig.se>
2015-12-28 13:50:47 +0100

    testpush

--- ProjectX/WebContent/js/building.js
+++ ProjectX/WebContent/js/building.js
@@ -1,28 +1,25 @@
 angular.module('projectx.building', [])
-.controller('buildingcontroller', ['$scope', function($scope, $http) {
-	
-	$scope.getData=function(){
-		$http({
-			method: 'GET',
-			url:'/api/Building'
-			
-		})
-		.success(function(data){
-			$scope.data=data;
-		})
-		.error(function(){
-			$scope.data={};
-		});
-	};
-	$scope.building={}
-	
-}])
 .directive('building', function(){
 	return {
 		restrict: 'E',
 		replace: 'true',
-		
 		templateUrl: 'templates/building.html',
+		controller: function($scope, $http) {
+			$scope.getData=function(){
+				$http({
+					method: 'GET',
+					url:'/ProjectX/api/Building'
+					
+				})
+				.success(function(data){
+					$scope.data=data;
+				})
+				.error(function(){
+					$scope.data={};
+				});
+			};
+			$scope.building={}
+		},
 		link:function(scope, element, attrs){
 			scope.getData();
 		}
--- ProjectX/src/se/hig/student/projectx/Building.java
+++ ProjectX/src/se/hig/student/projectx/Building.java
@@ -2,12 +2,16 @@ package se.hig.student.projectx;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.owlike.genson.Genson;
 
 /**
  * Servlet implementation class Building
@@ -25,11 +29,22 @@ public class Building extends HttpServlet {
 		response.setContentType("application/json;charset=UTF-8");
 		
 		try (PrintWriter out = response.getWriter()) {
-			out.print("{"
-					+ "'cities' : ['Gävle', 'Stockholm'],"
-					+ "'yearspans' : ['-1920', '1921-1940'],"
-					+ "'buildingtypes' : ['Bostad', Lokal'],"
-					+ "}");
+			TreeMap<String, List<String>> mappen = new TreeMap<>();
+			List<String> cities = new ArrayList<>();
+			List<String> yearspans = new ArrayList<>();
+			List<String> buildingtypes = new ArrayList<>();
+			cities.add("Gävle");
+			cities.add("Stockholm");
+			yearspans.add("-1920");
+			yearspans.add("1921-1940");
+			buildingtypes.add("Bostad");
+			
+			buildingtypes.add("Lokal");
+			mappen.put("cities", cities);
+			mappen.put("yearspans", yearspans);
+			mappen.put("buildingtypes", buildingtypes);
+			Genson genson = new Genson();
+			out.print(genson.serialize(mappen));
 		}
 	}
 
